adc54e68eef7243d824d974338cb78268c3cf160
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-19 11:44:34 -0600

    Add: proyect structure

---
+++ src/app/modules/property/property.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PropertyRoutingModule } from './property-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PropertyRoutingModule
+  ]
+})
+export class PropertyModule { }
