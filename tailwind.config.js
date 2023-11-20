64b8ad19dbe2f55276a15c3edd7693e399e18841
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-20 04:05:31 -0600

    Upd: Create Agendalo Color Palette in Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Agendalo Color Palette (Retro)
+        "agendalo-orange-700": '#ff3618',
+        "agendalo-orange-500": '#ff6c2e',
+        "agendalo-orange-300": '#ffae6c',
+        "agendalo-orange-100": '#fcddaf',
+        "agendalo-blue-100": '#71afa4',
+        "agendalo-blue-300": '#058d9d',
+        "agendalo-blue-500": '#016d93',
+        "agendalo-blue-700": '#011f51',
+      }
+    },
   },
   plugins: [],
 }
