ee990ab215cdbc64461e62aceb51715147c0dba7
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-20 07:28:10 -0600

    Upd: Styles SCSS

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
