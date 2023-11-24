89b981906b8ddca4824aca75f5f591d5c96392d3
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-23 23:03:46 -0600

    Add: Learning Angular Fetch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 
@@ -11,7 +12,8 @@ import { NgModule } from '@angular/core';
     BrowserModule,
     AppRoutingModule,
     NavBarComponent,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
